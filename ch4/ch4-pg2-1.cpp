2ea781f46354069d51300e3fe84ca92fa2e334ee
shuishiyuan <liubinbupt@qq.com>
2019-03-16 20:20:27 +0900

    In progress: combinatory

--- ch4/ch4-pg2-1.cpp
+++ ch4/ch4-pg2-1.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+long factorial(int n) {
+    long retVal = 1;
+    while (n > 0) {
+        retVal *= n--;
+    }
+    return retVal;
+}
+
+long combinatory(int n, int m) {
+    //return factorial(n) / (factorial(m) * factorial(n - m));
+    return factorial(n) / factorial(m) / factorial(n - m);
+}
+
 int main() {
     printf("Combinatory 1st!\n");
+    int n;
+    int m;
+    scanf("%d%d", &n, &m);
+    printf("%ld\n", combinatory(n, m));
     return 0;
 }
