049731e34bcf4bdeb00815c27420fb5515d41c18
shuishiyuan <liubinbupt@qq.com>
2019-03-18 06:03:04 +0900

    Finished: prime number

--- ch4/ch4-pg4.cpp
+++ ch4/ch4-pg4.cpp
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
-// the input n should nether be 1 nor be too big!
 int is_prime(int n) {
-    for (int i = 2; i * i <= n; ++i) {
+    if (n <= 1) {
+        return 0;
+    }
+
+    int n_border = floor(sqrt(n) + 0.5);
+    for(int i = 2; i <= n_border; ++i) {
         if (n % i == 0) {
             return 0;
         }
     }
+
     return 1;
 }
 
 int main() {
-    printf("prime number 1st(the input n should nether be 1 nor be too big!)\n");
+    printf("prime number 2nd!\n");
     int n;
-    while(scanf("%d", &n) == 1 && n > 0) {
+    while(scanf("%d", &n) == 1 && n != 0) {
         printf("%d is %sa prime number!\n", n, is_prime(n) ? "" : "NOT ");
     }
     return 0;
