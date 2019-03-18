bc14b82db6aa029e516dc7d3903e9e59df458fae
shuishiyuan <liubinbupt@qq.com>
2019-03-18 15:52:16 +0900

    Finished: swap

--- ch4/ch4-pg6.cpp
+++ ch4/ch4-pg6.cpp
@@ -6,12 +6,22 @@ void swap(int *a, int *b) {
     *a = *a - *b;
 }
 
+void swap_ng(int *a, int *b) {
+    int *temp;
+    //int temp;
+    *temp = *a;
+    *a = *b;
+    *b = *temp;
+}
+
 int main() {
     printf("swap success!\n\n");
-    int a = 3;
-    int b = 4;
+    int a = (unsigned int)(~0) >> 1;
+    int b = ((unsigned int)(~0) >> 1) + 1;
     printf("At first:\n%d\t%d\n\n", a, b);
     swap(&a, &b);
-    printf("After swap:\n%d\t%d\n", a, b);
+    printf("After swap:\n%d\t%d\n\n", a, b);
+    swap_ng(&a, &b);
+    printf("After swap_ng:\n%d\t%d\n", a, b);
     return 0;
 }
