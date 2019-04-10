1a98c707f9b3c53cab6dcae2d5a816209fb82338
shuishiyuan <liubinbupt@qq.com>
2019-04-10 18:45:53 +0900

    One Approach done: Ancient Cipher, NEERC 2004, UVa1339

--- ch4/ch4-pg10-exam1.cpp
+++ ch4/ch4-pg10-exam1.cpp
@@ -1,5 +1,28 @@
 #include<stdio.h>
 
+void showArray(int n[]) {
+    for (int i = 0; i < 26; ++i) {
+        printf("%d", n[i]);
+    }
+    putchar('\n');
+}
+
+bool decipherable(int a[], int b[]) {
+    for (int i = 0; i < 26; ++i) {
+        int j;
+        for (j = 0; j < 26; ++j) {
+            if (a[(j + i) % 26] != b[j]) {
+                break;
+            }
+        }
+        if (j == 26) {
+            printf("%d\n", i);
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     printf("Ancient Cipher, NEERC 2004, UVa1339\n");
     int cnt1[26] = {0};
@@ -14,12 +37,8 @@ int main() {
     while((c = getchar()) != '\n') {
         cnt2[c - 'A']++;
     }
-    for (int i = 0; i < 26; ++i) {
-        printf("%d", cnt1[i]);
-    }
-    putchar('\n');
-    for (int i = 0; i < 26; ++i) {
-        printf("%d", cnt2[i]);
-    }
+    showArray(cnt1);
+    showArray(cnt2);
+    printf("%s\n", decipherable(cnt1, cnt2) ? "YES" : "NO");
     return 0;
 }
