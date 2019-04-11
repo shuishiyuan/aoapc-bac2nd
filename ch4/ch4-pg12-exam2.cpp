0ff8e9623b50834e6eda7a6a105f7c513753a99b
shuishiyuan <liubinbupt@qq.com>
2019-04-11 17:07:25 +0900

    Finished: Hangman Judge, UVa489

--- ch4/ch4-pg12-exam2.cpp
+++ ch4/ch4-pg12-exam2.cpp
@@ -2,31 +2,32 @@
 
 int main() {
     printf("Hangman Judge, UVa489\n\n");
-    int round;
+    int rnd;
 #ifdef LOCAL
     freopen("./data/ch4-pg12-exam2.in", "r", stdin);
 #endif
-    while(scanf("%d\n", &round) == 1 && round != -1) {
+    while(scanf("%d\n", &rnd) == 1 && rnd != -1) {
         int n[26] = {0};
-        int counter = 0;
+        int cnt = 0;
         int wronger = 0;
         char c;
         while((c = getchar()) != '\n') {
             if (n[c - 'a']++ == 0) {
-                counter++;
+                cnt++;
             }
         }
         while((c = getchar()) != '\n') {
-            if (counter > 0 && wronger < 7) {
+            if (cnt > 0 && wronger < 7) {
                 if (n[c - 'a'] > 0) {
-                    counter--;
+                    cnt--;
+                    n[c - 'a'] = 0;
                 } else {
                     wronger++;
                 }
             }
         }
-        printf("Round %d\n", round);
-        if (counter == 0) {
+        printf("Round %d\n", rnd);
+        if (cnt == 0) {
             printf("You Win.\n");
         } else if (wronger == 7) {
             printf("You Lose.\n");
