9cb7a44262c9a95cc2302ef84da38f2291296053
shuishiyuan <liubinbupt@qq.com>
2019-06-13 16:54:45 +0800

    In progress: Message Decoding

--- ch4/ch4-pg14-exam4.cpp
+++ ch4/ch4-pg14-exam4.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 #define MAX_SIZE 100
 
 char decode_book[MAX_SIZE];
+
+int calculate_base_index(int base_num) {
+    int base_index = 0;
+    while (--base_num >= 0 ) {
+        base_index += pow(2, base_num);
+    }
+    return base_index;
+}
 
 int main() {
     printf("Message Decoding, ACM/ICPC World Finals 1991, UVa 213!\n");
@@ -15,12 +24,23 @@ int main() {
     scanf("%s\n", decode_book);
     printf("%s\n", decode_book);
 
+    int base_num = 0;
     int base_index = 0;
     int offset_index = 0;
     int break_step = 3;
+    bool is_book_ready = false;
     while((c = getchar()) != '\n') {
         if (break_step = 0) {
-            putchar(decode_book[base_index + offset_index]);
+            if (is_book_ready) {
+                putchar(decode_book[base_index + offset_index]);
+            } else {
+                if (base_num == 0) {
+                    return 0;
+                }
+                base_index = calculate_base_index(base_num);
+            }
+        } else {
+            ;
         }
     }
     putchar('\n');
