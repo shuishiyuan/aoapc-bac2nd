c127a4b469665b5ae6750e13840f7c0834c8676c
shuishiyuan <liubinbupt@qq.com>
2019-06-18 23:33:43 +0800

    Change the name of input file : Message Decoding

--- ch4/ch4-pg14-exam4.cpp
+++ ch4/ch4-pg14-exam4.cpp
@@ -17,7 +17,7 @@ int main() {
     printf("Message Decoding, ACM/ICPC World Finals 1991, UVa 213!\n");
 
 #ifdef LOCAL
-    freopen("data/ch4-pg14-exam4.in", "r", stdin);
+    freopen("data/ch4-exam4.in", "r", stdin);
 #endif
 
     char c;
