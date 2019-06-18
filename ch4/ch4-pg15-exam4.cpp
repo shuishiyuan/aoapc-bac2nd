83c9086f6f701fdc3c6fe6ca5df2a8106605be1e
shuishiyuan <liubinbupt@qq.com>
2019-06-19 07:52:07 +0800

    Finished: Message Decoding -- Another Approach

--- ch4/ch4-pg15-exam4.cpp
+++ ch4/ch4-pg15-exam4.cpp
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#define MAX_ROW 10
 
-char book[][];
+char book[MAX_ROW][1 << MAX_ROW];
 
 void read_book() {
     printf("Initializing the decoding book\n");
+    for (int book_row = 1; book_row < MAX_ROW; ++book_row) {
+        for (int book_col = 0; book_col < (1 << book_row) - 1; ++ book_col) {
+            char c = getchar();
+            if (c == '\n') {
+                return;
+            }
+            book[book_row][book_col] = c;
+        }
+    }
 }
 
 int read_digit(int bit) {
@@ -16,6 +26,7 @@ int read_digit(int bit) {
 
 int main() {
     printf("Message Decoding -- Another Approach\n");
+    freopen("data/ch4-exam4.in", "r", stdin);
     read_book();
     while (true) {
         int book_row = read_digit(3);
@@ -24,7 +35,7 @@ int main() {
         }
         while (true) {
             int book_col = read_digit(book_row);
-            if (book_col == 1 << book_row -1) {
+            if (book_col == (1 << book_row) - 1) {
                 break;
             }
             putchar(book[book_row][book_col]);
