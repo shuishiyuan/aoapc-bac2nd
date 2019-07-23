d7951e63552076857bc6aa8946743f2231e39041
b-liu <b-liu@nri.co.jp>
2019-07-23 16:23:12 +0800

    Started: Solution a of Spreadsheet Tracking!

--- ch4/ch4-pg16-exam5.cpp
+++ ch4/ch4-pg16-exam5.cpp
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#define MAX 50
 
 int main() {
     printf("Solution a of Spreadsheet Tracking!\n");
+    int work_sheet[MAX][MAX];
+    int row_num;
+    int col_num;
+    int operation_num;
+    int query_num;
+
+#ifdef LOCAL
+    freopen("data\ch4-exam5.in", "r", stdin);
+#endif
+    scanf("%d%d\n", &row_num, &col_num);
+    scanf("%d\n", &operation_num);
+    while(operation_num--) {
+        ;
+    }
+
+    int row_pos;
+    int col_pos;
+    scanf("%d\n", &query_num);
+    while(query_num--) {
+        scanf("%d%d\n", &row_pos, &col_pos);
+        printf("%d\t%d\n", row_pos, col_pos);
+    }
     return 0;
 }
