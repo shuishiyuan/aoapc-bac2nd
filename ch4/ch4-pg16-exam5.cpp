857456a6b9e0cee147950a1f324b1a0f41dac016
b-liu <b-liu@nri.co.jp>
2019-08-09 17:11:38 +0800

    cycle1 compile ok: Solution a of Spreadsheet Tracking!

--- ch4/ch4-pg16-exam5.cpp
+++ ch4/ch4-pg16-exam5.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 50
 
 int main() {
@@ -10,12 +11,22 @@ int main() {
     int query_num;
 
 #ifdef LOCAL
-    freopen("data\ch4-exam5.in", "r", stdin);
+    freopen("data/ch4-exam5.in", "r", stdin);
 #endif
     scanf("%d%d\n", &row_num, &col_num);
     scanf("%d\n", &operation_num);
+    char operation_key[3];
+    char current_key[3];
+    scanf("%s", operation_key);
+    printf("\n%s ", operation_key);
     while(operation_num--) {
-        ;
+        scanf("%s", current_key);
+        while(current_key[0] > '0' && current_key[0] < '9') {
+            printf("%s", current_key);
+            scanf("%s", current_key);
+        }
+        strcpy(current_key, operation_key);
+        printf("\n%s", operation_key);
     }
 
     int row_pos;
@@ -24,7 +35,7 @@ int main() {
     scanf("%d\n", &query_num);
     while(query_num--) {
         scanf("%d%d\n", &row_pos, &col_pos);
-        printf("%d\t%d\n", row_pos, col_pos);
+        printf("%d %d\n", row_pos, col_pos);
     }
     return 0;
 }
