70c8d85ad20f6f07e7354376f09865a02936784c
shuishiyuan <liubinbupt@qq.com>
2020-03-14 17:02:41 +0800

    Fixed the read_char bug by using the hexdecimal number to blank characters

--- ch4/ch4-pg17-exam5.cpp
+++ ch4/ch4-pg17-exam5.cpp
@@ -6,18 +6,15 @@
 int row_num;
 int col_num;
 int prc_num;
-
-
 int *prc;
 
 bool read_char(char *res) {
-    printf("reading chars\n");
     char c;
-    while((c = getchar()) != 20 && c != '\n') {
+    while((c = getchar()) != 0x20 && c != 0x0a) {
         *res++ = c;
     }
-    *res = '\0';
-    if (c == '\n') {
+    *res = 0x00;
+    if (c == 0x0a) {
         return false;
     }
     return true;
@@ -29,18 +26,21 @@ int main() {
     freopen("data/ch4-exam5.in", "r", stdin);
 #endif
     scanf("%d %d", &row_num, &col_num);
-    scanf("%d\n", &prc_num);
+    scanf("%d", &prc_num);
     printf("The input row number is: %d\n", row_num);
     printf("The input col number is: %d\n", col_num);
     printf("The input process number is: %d\n", prc_num);
     prc = (int*)malloc(prc_num * MAX * sizeof(int));
     char key[3];
-    // key = (char*)malloc(3 * sizeof(char));
     for (int i = 0; i < prc_num; ++i) {
         while (read_char(key)) {
-            printf("%s", key);
+            printf("%s ", key);
         }
-        putchar('\n');
+        if (key[0] == 0x00) {
+            --i;
+            continue;
+        }
+        printf("%s\n", key);
     }
     return 0;
 }
