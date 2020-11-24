57088aa7cdf4ca057a6be4154704a7ba9fb07775
b-liu <b-liu@nri.co.jp>
2020-11-24 08:25:04 +0800

    Started:The PI / 4 approximate value Problem

--- ch2/ch2-pg6-exam3.js
+++ ch2/ch2-pg6-exam3.js
@@ -1,1 +1,18 @@
 console.log("The PI / 4 approximate value Problem");
+
+let sum = 1;
+let entry = 1;
+
+let i = 1;
+do {
+    entry = 1 / (i * 2 + 1);
+    if (i % 2 == 1) {
+        sum -= entry;
+    } else {
+        sum += entry;
+    }
+    i++;
+// } while(entry > 1e-6)
+} while(entry > 0.0001)
+
+console.log(`The approximated value of PI / 4 is: ${sum}`);
