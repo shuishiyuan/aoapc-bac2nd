3a8807655fdbae89f9a907ee228ecf3c8b725870
b-liu <b-liu@nri.co.jp>
2020-11-26 08:31:37 +0800

    In Progress: The sum of the factorial

--- ch2/ch2-pg7-exam4.js
+++ ch2/ch2-pg7-exam4.js
@@ -1,1 +1,12 @@
 console.log("The sum of the factorial");
+const { prependOnceListener } = require("process");
+const readLine = require("readline");
+const cmdLineReader = readLine.createInterface({
+    input: process.stdin
+});
+cmdLineReader.on('close', () => {
+    process.exit()
+});
+
+let n = 0;
+let sum = 0;
