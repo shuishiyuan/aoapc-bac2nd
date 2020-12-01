cc788ec0eeb0eb79bb4ffd5af8a9388fb53728f5
b-liu <b-liu@nri.co.jp>
2020-12-01 08:04:40 +0800

    Finished: The sum of the factorial: in recursion

--- ch2/ch2-pg8-exam4.js
+++ ch2/ch2-pg8-exam4.js
@@ -15,12 +15,13 @@ let processFractional = (n) => {
     }
     return n * processFractional(n - 1);
 }
- let processSum = (n) => {
-     if (n === 1) {
-         return 1;
-     }
-     return processFractional(n) + processSum(n - 1);
- }
+
+let processSum = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+    return processFractional(n) + processSum(n - 1);
+}
 
 cmdLineReader.on('line', (input) => {
     n = parseInt(input);
@@ -28,8 +29,5 @@ cmdLineReader.on('line', (input) => {
         cmdLineReader.close();
     }
     const sum = processSum(n);
-    // for (let i = 1; i <= n; i++) {
-        // sum += processFractional(i);
-    // }
     console.log(`The sum of the fractional from 1 to ${n} is :${sum}`);
 })
