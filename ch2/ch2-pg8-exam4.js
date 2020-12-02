b53f2712d838eaa8954c0dc46eb6e3a9446482a8
b-liu <b-liu@nri.co.jp>
2020-12-02 19:59:33 +0800

    Started: The sum of the factorial: in a crazy way

---
+++ ch2/ch2-pg8-exam4.js
@@ -0,0 +1,34 @@
+console.log("The sum of the factorial: in recursion");
+const lineReader = require("readline");
+const cmdLineReader = lineReader.createInterface({
+    input: process.stdin
+});
+cmdLineReader.on('close', () => {
+    process.exit()
+});
+
+let n = 0;
+
+let processFractional = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+    return n * processFractional(n - 1);
+}
+
+let processSum = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+    return processFractional(n) + processSum(n - 1);
+}
+
+cmdLineReader.on('line', (input) => {
+    n = parseInt(input);
+    if (n === 0) {
+        cmdLineReader.close();
+    }
+    const sum = processFractional(n);
+    // const sum = processSum(n);
+    console.log(`The sum of the fractional from 1 to ${n} is :${sum}`);
+})
