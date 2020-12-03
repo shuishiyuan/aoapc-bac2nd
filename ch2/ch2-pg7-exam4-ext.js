1494ace48dcf3b3b582b86b8dea366ddbf0efa07
b-liu <b-liu@nri.co.jp>
2020-12-03 08:28:47 +0800

    In progress: The sum of the factorial: six digit refactoring

--- ch2/ch2-pg7-exam4-ext.js
+++ ch2/ch2-pg7-exam4-ext.js
@@ -7,13 +7,15 @@ cmdLineReader.on('close', () => {
     process.exit()
 });
 
+const MOD = 1000000;
 let n = 0;
+let counter = 1;
 
 let processFractional = (n) => {
     if (n === 1) {
         return 1;
     }
-    return n * processFractional(n - 1);
+    return n * processFractional(n - 1) % MOD;
 }
 
 let processSum = (n) => {
@@ -23,11 +25,15 @@ let processSum = (n) => {
     return processFractional(n) + processSum(n - 1);
 }
 
+console.time('fractional-process-time');
 cmdLineReader.on('line', (input) => {
     n = parseInt(input);
     if (n === 0) {
         cmdLineReader.close();
     }
+    // const sum = processFractional(n) % MOD;
     const sum = processSum(n);
     console.log(`The sum of the fractional from 1 to ${n} is :${sum}`);
+    console.timeLog('fractional-process-time', sum);
+    counter++
 })
