554da88c7a8f7669b85ec7ae877b8e567c18fde1
b-liu <b-liu@nri.co.jp>
2020-12-09 19:45:01 +0800

    In Progress: Number data statics1 with file readline

--- ch2/ch2-pg10-exam5.js
+++ ch2/ch2-pg10-exam5.js
@@ -1,1 +1,21 @@
 console.log("Number data statics1 with file readline");
+const fs = require("fs");
+const { stdout } = require("process");
+const lineReader = require("readline");
+const fileLineReader = lineReader.createInterface({
+    input: fs.createReadStream("data/ch2-pg10-exam5.in"),
+    // input: process.stdin,
+    output: fs.createWriteStream("data/ch2-pg10-exam5.out")
+    // output: process.stdout
+});
+fileLineReader.on('close', () => {
+    process.exit()
+});
+
+fileLineReader.on('line', (input) => {
+    digit = parseInt(input);
+    if (digit === 0) {
+        fileLineReader.close()
+    }
+    console.log(`the input from file is ${digit}`);
+});
