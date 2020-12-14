540e69a9cf08f4c816bad06eca37aeadd0efd1ff
b-liu <b-liu@nri.co.jp>
2020-12-14 19:13:02 +0800

    Debugging: Number data statics with async await

--- ch2/ch2-pg11-exam5.js
+++ ch2/ch2-pg11-exam5.js
@@ -6,11 +6,35 @@ const rs = fs.createReadStream("data/ch2-pg11-exam5.in");
 const ws = fs.createWriteStream("data/ch2-pg11-exam5.out");
 
 const rl = require("readline");
-async processLine = () => {
+const { once } = require("process");
+
+let digit = 0;
+let sum = 0;
+let counter = 0;
+let max = 0;
+let min = 0;
+// async function processLine() {
+(async () => {
     const frl = rl.createInterface({
         input: rs
     });
-    await for (input of frl) {
-        console.log(`The input of file is ${input}`);
+
+    for await (const input of frl) {
+        counter++;
+        digit = parseInt(input);
+        sum += digit;
+        if (digit > max) {
+            max = digit;
+        }
+        if (digit < min) {
+            min = digit;
+        }
     }
-};
+    // await once(frl, 'close');
+    // await once('beforeExit', () => {
+    once('beforeExit', () => {
+        console.log(`Counter:${counter}\tAverage:${(sum / counter).toFixed(3)}\tMax:${max}\tMin:${min}`);
+        ws.write(`Counter:${counter}\tAverage:${(sum / counter).toFixed(3)}\tMax:${max}\tMin:${min}`);
+    });
+})();
+// processLine();
