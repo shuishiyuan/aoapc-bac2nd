d37611c3f906f8db95647d45e31ceaa26fc6b6f4
b-liu <b-liu@nri.co.jp>
2020-12-21 19:38:09 +0800

    Debugging: Number data statics2

--- ch2/ch2-pg12-exam6.js
+++ ch2/ch2-pg12-exam6.js
@@ -9,11 +9,35 @@ rl.on('close', () => {
     process.exit()
 });
 
-let counter = 0;
+let counter = NaN;
+let sum = 0;
+let max = Number.MIN_SAFE_INTEGER;
+let min = Number.MAX_SAFE_INTEGER;
+let index = 0;
 const processLine = async () => {
-    await counter = parseInt(input);
-    if (counter === 0 ) {
-        rl.close();
+    for await (const input of rl) {
+        if (input == 0) {
+            rl.close();
+        } else if (counter === NaN) {
+            counter = parseInt(input);
+            sum = 0;
+            max = Number.MIN_SAFE_INTEGER;
+            min = Number.MAX_SAFE_INTEGER;
+            index = counter;
+        } else if (index != 0) {
+            let digit = parseInt(input);
+            sum += digit;
+            if (digit > max) {
+                max = digit;
+            }
+            if (digit < min) {
+                min = digit;
+            }
+            index--;
+        } else {
+            console.log(`max: ${max}\tmin: ${min}\taverage: ${(sum / counter).toFixed(3)}`);
+            counter = NaN;
+        }
     }
 }
 processLine();
