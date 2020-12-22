12e7419d7885dd893f307abb7e264a0abf228c77
b-liu <b-liu@nri.co.jp>
2020-12-22 08:22:27 +0800

    Communication OK: Number data statics2

--- ch2/ch2-pg12-exam6.js
+++ ch2/ch2-pg12-exam6.js
@@ -1,6 +1,7 @@
 console.log("Number data statics2");
 const { count } = require("console");
 const { realpath } = require("fs");
+const { join } = require("path");
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin
@@ -9,7 +10,7 @@ rl.on('close', () => {
     process.exit()
 });
 
-let counter = NaN;
+let counter = undefined;
 let sum = 0;
 let max = Number.MIN_SAFE_INTEGER;
 let min = Number.MAX_SAFE_INTEGER;
@@ -18,13 +19,13 @@ const processLine = async () => {
     for await (const input of rl) {
         if (input == 0) {
             rl.close();
-        } else if (counter === NaN) {
+        } else if (counter === undefined) {
             counter = parseInt(input);
             sum = 0;
             max = Number.MIN_SAFE_INTEGER;
             min = Number.MAX_SAFE_INTEGER;
             index = counter;
-        } else if (index != 0) {
+        } else if (index > 0) {
             let digit = parseInt(input);
             sum += digit;
             if (digit > max) {
@@ -34,9 +35,10 @@ const processLine = async () => {
                 min = digit;
             }
             index--;
-        } else {
-            console.log(`max: ${max}\tmin: ${min}\taverage: ${(sum / counter).toFixed(3)}`);
-            counter = NaN;
+            if (index === 0) {
+                console.log(`counter: ${counter}\tmax: ${max}\tmin: ${min}\taverage: ${(sum / counter).toFixed(3)}`);
+                counter = undefined;
+            }
         }
     }
 }
