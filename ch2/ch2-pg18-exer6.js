608145978b2254d7a1c474e9531d871b2fda67eb
b-liu <b-liu@nri.co.jp>
2021-01-05 19:23:56 +0800

    In progress: Permutation

--- ch2/ch2-pg18-exer6.js
+++ ch2/ch2-pg18-exer6.js
@@ -1,1 +1,15 @@
 console.log("Permutation");
+
+let curDigiArr = [];
+const consDigits = () => {
+    for (let i = 1; i <= 9; ++i) {
+        if (curDigiArr.length == 9) {
+            console.log(`The digit array is ${curDigiArr}`);
+            process.exit()
+        }
+        if (curDigiArr.indexOf('' + i)) {
+            continue;
+        }
+        consDigits();
+    }
+}
