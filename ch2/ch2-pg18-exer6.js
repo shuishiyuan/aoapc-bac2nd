7a88f55b4e8b3e0b56ad9c4cdfb328374193009b
b-liu <b-liu@nri.co.jp>
2021-01-06 08:27:25 +0800

    Debugging: Permutation

--- ch2/ch2-pg18-exer6.js
+++ ch2/ch2-pg18-exer6.js
@@ -2,11 +2,26 @@ console.log("Permutation");
 
 let curDigiArr = '';
 // let curDigiArr = [];
+const isPermutation = () => {
+    let rtnValue = false;
+    const a = parseInt(curDigiArr.substr(0, 3));
+    const b = parseInt(curDigiArr.substr(3, 3));
+    const c = parseInt(curDigiArr.substr(6, 3));
+    if (b * b == a * c) {
+        rtnValue = true;
+    }
+    return rtnValue;
+}
+
 const consDigits = () => {
     for (let i = 1; i <= 9; ++i) {
         if (curDigiArr.length == 9) {
-            console.log(`The digit array is ${curDigiArr}`);
-            process.exit();
+            if (isPermutation()) {
+                console.log(`The digit array is ${curDigiArr}`);
+            }
+            // curDigiArr = '';
+            return;
+            // process.exit();
         }
         if (curDigiArr.indexOf(i) != -1) {
         // if (curDigiArr.indexOf('' + i)) {
@@ -15,6 +30,7 @@ const consDigits = () => {
         curDigiArr += i;
         // curDigiArr.push('' + i);
         consDigits();
+        curDigiArr = '';
     }
 }
 consDigits();
