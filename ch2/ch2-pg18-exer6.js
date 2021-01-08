3c5d5c9078765d3ea714d511f1b11943a91bb3ad
b-liu <b-liu@nri.co.jp>
2021-01-08 22:39:44 +0800

    Debugging: push pop approach of  Permutation

--- ch2/ch2-pg18-exer6.js
+++ ch2/ch2-pg18-exer6.js
@@ -1,6 +1,7 @@
 console.log("Permutation");
 
-let curDigiArr = '';
+let curDigiArr = [];
+// let curDigiArr = '';
 // let layerCnt = 0;
 const consDigits = () => {
     // layerCnt++;
@@ -12,17 +13,19 @@ const consDigits = () => {
         if (curDigiArr.indexOf(i) != -1) {
             continue;
         }
-        curDigiArr += i;
+        curDigiArr.push(i);
+        // curDigiArr += i;
         consDigits();
         // layerCnt--;
-        curDigiArr = curDigiArr.substr(0, curDigiArr.length - 1);
+        curDigiArr.pop();
+        // curDigiArr = curDigiArr.substr(0, curDigiArr.length - 1);
     }
 }
 
 const judgePermutation = () => {
-    const a = parseInt(curDigiArr.substr(0, 3));
-    const b = parseInt(curDigiArr.substr(3, 3));
-    const c = parseInt(curDigiArr.substr(6, 3));
+    const a = parseInt(curDigiArr.toString().substr(0, 3));
+    const b = parseInt(curDigiArr.toString().substr(3, 3));
+    const c = parseInt(curDigiArr.toString().substr(6, 3));
     const aDivideB = (a / b).toFixed(4);
     const bDivideC = (b / c).toFixed(4);
     if (aDivideB == bDivideC) {
