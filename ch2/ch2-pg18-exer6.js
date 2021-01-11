054fa9d21a7bf958c9f15cd0f57083ee074bc74e
b-liu <b-liu@nri.co.jp>
2021-01-11 18:47:04 +0800

    Recovery: version-control miss of Permutation

--- ch2/ch2-pg18-exer6.js
+++ ch2/ch2-pg18-exer6.js
@@ -1,14 +1,39 @@
-console.log("Print the value of 2,4,6,...,2n");
+console.log("Permutation");
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin
-});
-
-let n;
-
-(async () => {
-    for await (let input of rl) {
-        ;
+let curDigiArr = [];
+// let layerCnt = 0;
+const consDigits = () => {
+    // layerCnt++;
+    for (let i = 1; i <= 9; ++i) {
+        if (curDigiArr.length == 9) {
+            judgePermutation();
+            return;
+        }
+        if (curDigiArr.includes(i)) {
+            continue;
+        }
+        curDigiArr.push(i);
+        consDigits();
+        // layerCnt--;
+        curDigiArr.pop();
     }
-})();
+}
+
+const judgePermutation = () => {
+    const curDigiStr = curDigiArr.join('');
+
+    const a = parseInt(curDigiStr.substr(0, 3));
+    const b = parseInt(curDigiStr.substr(3, 3));
+    const c = parseInt(curDigiStr.substr(6, 3));
+
+    const aDivideB = (a / b).toFixed(4);
+    const bDivideC = (b / c).toFixed(4);
+
+    if (aDivideB == bDivideC) {
+        console.log(`The permutation digits are: ${a}\t${b}\t${c}`);
+    }
+}
+
+console.time("Permutation");
+consDigits();
+console.timeEnd("Permutation");
