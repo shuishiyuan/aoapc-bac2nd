b4152f0e1655b5a310a9875e4439ee9cc93da63e
b-liu <b-liu@nri.co.jp>
2021-01-25 08:30:13 +0800

    In progress Snake Matrics

--- ch3/ch3-pg3.js
+++ ch3/ch3-pg3.js
@@ -7,8 +7,23 @@ rl.on('close', () => {
     process.exit();
 });
 
-const goAhead = () => {
-    ;
+let snakArr;
+
+const goAhead = (n) => {
+    for (let i = 0; i < n; ++i) {
+        snakArr[i][n - 1] = i + 1;
+    }
+}
+
+const printArr = () => {
+    let output = '';
+
+    snakArr.forEach(rowElement => {
+        rowElement.forEach(colElement => {
+            output += colElement + '\t';
+        });
+        output += '\n';
+    });
 }
 
 (async () => {
@@ -16,5 +31,9 @@ const goAhead = () => {
         if (input == 'Q') {
             rl.close();
         }
+        let n = parseInt(input);
+        snakArr = [[]];
+        goAhead(n);
+        printArr();
     }
-})
+})();
