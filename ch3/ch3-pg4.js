f38150987c7eaa3625bb851fa8131178f8cee965
shuishiyuan <liubinbupt@qq.com>
2021-02-18 19:58:23 +0800

    In progress: Vertical Form Problem

--- ch3/ch3-pg4.js
+++ ch3/ch3-pg4.js
@@ -12,6 +12,16 @@ rl.on('close', () => {
             rl.close();
         };
         let s = input;
-        console.log(`There is no answer to the input: ${s}`);
+        let counter = 0;
+        let vc;
+        for(let abc = 111; abc <= 999; ++abc) {
+            for (let de = 11; de <= 99; ++de) {
+                vc = `${abc * (de % 10)}${abc * de}`;
+            }
+        }
+        console.log(`The last value of abc * de is ${vc}`);
+        if (!counter) {
+            console.log(`There is no answer to the input: ${s}`);
+        }
     };
 })();
