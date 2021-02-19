5af15d8c3a3d6a96ca3743261ccd7968142b190f
shuishiyuan <liubinbupt@qq.com>
2021-02-19 08:21:58 +0800

    In output painting: Vertical Form Problem

--- ch3/ch3-pg4.js
+++ ch3/ch3-pg4.js
@@ -6,6 +6,17 @@ const rl = readline.createInterface({
 rl.on('close', () => {
     process.exit();
 });
+const paint = (counter, abc, de) => {
+    console.log(`<${counter}>`);
+    console.log(`  ${abc}`);
+    console.log(`X  ${de}`);
+    console.log(`-----`);
+    console.log(`  ${abc * (de % 10)}`);
+    console.log(` ${abc * Math.floor(de / 10)}`);
+    console.log(`-----`);
+    console.log(` ${abc * de}`);
+}
+
 (async () => {
     for await (let input of rl) {
         if (input === 'Q') {
@@ -16,10 +27,12 @@ rl.on('close', () => {
         let vc;
         for(let abc = 111; abc <= 999; ++abc) {
             for (let de = 11; de <= 99; ++de) {
-                vc = `${abc * (de % 10)}${abc * de}`;
+                vc = `${abc}${de}${abc * (de % 10)}${abc * Math.floor(de / 10)}${abc * de}`;
             }
         }
-        console.log(`The last value of abc * de is ${vc}`);
+        paint(counter, 111, 11);
+        // paint(counter, abc, de);
+        // console.log(`The last value of abc * de is ${vc}`);
         if (!counter) {
             console.log(`There is no answer to the input: ${s}`);
         }
