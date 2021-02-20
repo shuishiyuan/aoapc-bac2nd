36cfc7f5418b20d18f65f03b9d0a7003618f27be
shuishiyuan <liubinbupt@qq.com>
2021-02-20 08:25:21 +0800

    Adjusting the sprintf alternative: Vertical Form Problem

--- ch3/ch3-pg4.js
+++ ch3/ch3-pg4.js
@@ -1,4 +1,6 @@
-console.log(`Vertical Form Problem`);
+const util = require("util");
+util.log(`Vertical Form Problem`);
+util.debuglog(`Vertical Form Problem`);
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin
@@ -7,14 +9,15 @@ rl.on('close', () => {
     process.exit();
 });
 const paint = (counter, abc, de) => {
-    console.log(`<${counter}>`);
-    console.log(`  ${abc}`);
-    console.log(`X  ${de}`);
-    console.log(`-----`);
-    console.log(`  ${abc * (de % 10)}`);
-    console.log(` ${abc * Math.floor(de / 10)}`);
-    console.log(`-----`);
-    console.log(` ${abc * de}`);
+    let output = util.format('<%d>\n', counter);
+    output += util.format('%s%d\n', '  ', abc);
+    // util.format(`X${de}`);
+    // util.format(`-----`);
+    // util.format(`${abc * (de % 10)}`);
+    // util.format(`${abc * Math.floor(de / 10)}`);
+    // util.format(`-----`);
+    // util.format(`${abc * de}`);
+    console.log(output);
 }
 
 (async () => {
@@ -24,6 +27,7 @@ const paint = (counter, abc, de) => {
         };
         let s = input;
         let counter = 0;
+        paint(counter, 999, 99);
         let vc;
         for (let abc = 111; abc <= 999; ++abc) {
             for (let de = 11; de <= 99; ++de) {
@@ -42,7 +46,8 @@ const paint = (counter, abc, de) => {
             }
         }
         if (!counter) {
-            console.log(`There is no answer to the input: ${s}`);
+            // console.log(`There is no answer to the input: ${s}`);
+            util.log(`There is no answer to the input: ${s}`);
         }
     };
 })();
