dab69a02633709d7d6d9e48758a1c5a93c450800
shuishiyuan <liubinbupt@qq.com>
2021-03-03 19:55:38 +0800

    Compiling: UVa272 Tex Quotes

--- ch3/ch3-pg5-exam1.js
+++ ch3/ch3-pg5-exam1.js
@@ -3,3 +3,30 @@ const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin
 });
+rl.on('close', () => {
+    process.exit();
+});
+
+(async () => {
+    for  await (let input of rl) {
+        if (input === 'Q') {
+            rl.close();
+        }
+        for (let i = 0; i < input.lentgh; ++i) {
+            let c = input.charAtIndex(i);
+            let quoting = false;
+            if (c === '\"') {
+                if (!quoting) {
+                    s += '``';
+                    quoting = true;
+                } else {
+                    s += '\'\''
+                    quoting = false;
+                }
+            } else {
+                s += c;
+            }
+        }
+        console.log(new Date().toUTCString(), s);
+    }
+})();
