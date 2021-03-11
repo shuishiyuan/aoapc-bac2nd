d740428dfca131d28675e8acdfad0b881f351c87
shuishiyuan <liubinbupt@qq.com>
2021-03-11 08:24:16 +0800

    Debugging: WERTYU own way

--- ch3/ch3-pg6-exam2.js
+++ ch3/ch3-pg6-exam2.js
@@ -1,1 +1,29 @@
-console.log(new Date().toDateStringd(), 'WERTYU')
+console.log(new Date().toDateString(), 'WERTYU')
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin
+});
+rl.on('close', () => {
+    process.exit()
+});
+
+const DIC = '1234567890-=!@#$%^&*()_+';
+
+(async () => {
+    for await (let line of rl) {
+        if (line === 'Q') {
+            rl.close();
+        }
+        let output = '';
+        for (let i = 0; i < line.length; ++i) {
+            let c = line.charAt(i);
+            if (DIC.includes(c)) {
+                output += DIC[DIC.indexOf(c) - 1];
+            } else {
+                output += c;
+            }
+        }
+        console.log(new Date().toUTCString(), output);
+    }
+});
