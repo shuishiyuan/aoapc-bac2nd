ba39c1d73f18078e33f580c6ea6fe9da53c1cb69
shuishiyuan <liubinbupt@qq.com>
2021-03-15 08:32:03 +0800

    In-Progress: UVa10082 WERTYU

--- ch3/UVa10082.js
+++ ch3/UVa10082.js
@@ -1,1 +1,22 @@
 ﻿console.log(new Date().toLocaleDateString(), "UVa10082 WERTYU"); 
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin
+});
+rl.on('close', () => {
+    process.exit();
+});
+
+const DIC = "`1234567890-=!@#$%^&*()_+";
+
+(async () => {
+    for await (let input of rl) {
+        if (input === 'Q') {
+            rl.exit();
+        }
+        for (let i = 0; i < input.length; ++i) {
+            let c = input(i);
+        }
+    };
+})();
