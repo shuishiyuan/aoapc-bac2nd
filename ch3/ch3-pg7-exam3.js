062033851ca73b7529f1f5c58cb5ef2ac292daf7
shuishiyuan <liubinbupt@qq.com>
2021-03-17 20:20:43 +0800

    In Progress: UVa401 Palindromes

--- ch3/ch3-pg7-exam3.js
+++ ch3/ch3-pg7-exam3.js
@@ -1,1 +1,22 @@
 console.log(new Date().toDateString(), "UVa401 Palindromes");
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin
+});
+rl.on('close', () => {
+    process.exit();
+});
+
+const RAW = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
+const MIR = '1-E-S67890QWERTYUIOPASDFGHJKLZXCVBNM';
+
+(async () => {
+    for await (let line of rl) {
+        if ('Q' === line) {
+            rl.close();
+        };
+        let isSymetry = 1;
+        let isPalindrome = 1;
+    }
+})();
