3b00c283168ed60ec75e06777ef88fbf09b0b50c
shuishiyuan <liubinbupt@qq.com>
2021-03-18 08:22:44 +0800

    In-Communication: UVa401 Palindromes

--- ch3/ch3-pg7-exam3.js
+++ ch3/ch3-pg7-exam3.js
@@ -1,5 +1,6 @@
 console.log(new Date().toDateString(), "UVa401 Palindromes");
 
+const sprintf = require("sprintf-js").sprintf;
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin
@@ -9,14 +10,45 @@ rl.on('close', () => {
 });
 
 const RAW = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
-const MIR = '1-E-S67890QWERTYUIOPASDFGHJKLZXCVBNM';
+const MIR = '1-E-S67890QWERTYUIOPA2DFGHJKLZXCVBNM';
+const MSG = [
+    "is neither Symetry nor Palindromes.",
+    "is Symetry but not Palindromes.",
+    "is not Symetry but Palindroes.",
+    "is both Symetry and Palindroes."
+];
 
 (async () => {
+    let output = '';
     for await (let line of rl) {
         if ('Q' === line) {
+            console.log(output);
             rl.close();
         };
         let isSymetry = 1;
         let isPalindrome = 1;
+
+        let ascInd = 0;
+        let descInd = line.length;
+        while (ascInd <= descInd) {
+            if (line.charAt(ascInd) != line.charAt(descInd)) {
+                isSymetry = 0;
+                break;
+            }
+            ascInd++;
+            descInd--;
+        }
+
+        ascInd = 0;
+        descInd = line.length;
+        while (ascInd <= descInd) {
+            if (line.charAt(ascInd) != MIR.charAt(RAW.indexOf(line.charAt(descInd)))) {
+                isPalindrome = 0;
+                break;
+            }
+            ascInd++;
+            descInd--;
+        }
+        output += sprintf('%s : %s\n', line, MSG[2 * isPalindrome + isSymetry]);
     }
 })();
