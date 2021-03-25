20860326bf83d7862bf917b30bddf8103db08f2f
shuishiyuan <liubinbupt@qq.com>
2021-03-25 08:25:41 +0800

    In-Progress: UVa401 Palindromes ina delegate way

--- ch3/UVa401.js
+++ ch3/UVa401.js
@@ -1,1 +1,29 @@
 ﻿console.log(new Date().toUTCString(), "UVa401 Palindromes in a delegate way");
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin
+});
+
+rl.on('close', () => {
+    process.exit();
+});
+
+const MIR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+
+(async () => {
+    for await (let line of rl) {
+        if ('Q' === line) {
+            rl.close();
+        }
+        let isSymetry = true;
+        let isPalindromes = true;
+        let len = line.length - 1;
+
+        for (let i = 0; i < (1 + len) / 2; ++i) {
+            if (line.indexOf(i) != line.indexOf(len - i)) {
+                isSymetry = false;
+            }
+        }
+    }
+})();
