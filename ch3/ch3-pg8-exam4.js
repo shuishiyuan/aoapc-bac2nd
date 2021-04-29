5e5b670f97e00e825ba3598ab80278ef04bea32b
shuishiyuan <liubinbupt@qq.com>
2021-04-29 08:32:40 +0800

    Start stacking some code of UVa340 Master-Mind Hints the 1st approach

--- ch3/ch3-pg8-exam4.js
+++ ch3/ch3-pg8-exam4.js
@@ -1,1 +1,18 @@
 console.log("UVa340 Master-Mind Hints the 1st approach");
+const fs = require("fs");
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: fs.read("data/ch3-pg8-exam4.in")
+});
+rl.on('close', () => {
+    process.exit();
+});
+
+(async () => {
+    for await (line of rl) {
+        if (line === 0){
+            rl.close();
+        }
+    }
+})();
