ddaae463f4276b893db1b22604cbb34d46825350
shuishiyuan <liubinbupt@qq.com>
2021-05-24 08:32:56 +0800

    start coding the UVa340 Master-Mind Hints the 1st approach.

--- ch3/ch3-pg8-exam4.js
+++ ch3/ch3-pg8-exam4.js
@@ -1,21 +1,23 @@
 console.log("UVa340 Master-Mind Hints the 1st approach");
 const fs = require("fs");
+const rs = fs.createReadStream("data/ch3-pg8-exam4.in");
+const ws = fs.createWriteStream("data/ch3-pg8-exam4-out");
 
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: fs.read("data/ch3-pg8-exam4.in")
+    input: rs
 });
 rl.on('close', () => {
     process.exit();
 });
 
 (async () => {
-    for await (line of rl) {
+    for await (let line of rl) {
         if (line === 0){
             rl.close();
         }
-        if (line === 'Q') {
-            ;
-        }
+        // if (line === 'Q') {
+            ws.write(line);
+        // }
     }
 })();
