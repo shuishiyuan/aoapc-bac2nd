497dced74a640e63e317e07e5a1c9f2b9b989611
shuishiyuan <liubinbupt@qq.com>
2021-05-31 21:14:08 +0800

    isGameBreak was wroten.

--- ch3/ch3-pg8-exam4.js
+++ ch3/ch3-pg8-exam4.js
@@ -4,6 +4,7 @@ const rs = fs.createReadStream("data/ch3-pg8-exam4.in");
 const ws = fs.createWriteStream("data/ch3-pg8-exam4.out");
 
 let output = '';
+let bitWidth = 0;
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -14,6 +15,15 @@ rl.on('close', () => {
     process.exit();
 });
 
+const isGameBreak = (line) => {
+    let cnt = bitWidth;
+    while (--cnt >= 0) {
+        if (line.charAt(cnt) != '0') {
+            return false;
+        }
+    }
+    return true;
+}
 
 (async () => {
     for await (let line of rl) {
@@ -21,6 +31,14 @@ rl.on('close', () => {
         output = output + line + '\n';
         if ('0' === line) {
             rl.close();
+        }
+        if (bitWidth === 0) {
+            bitWidth = parseInt(line);
+            continue;
+        }
+        if (isGameBreak(line)) {
+            bitWidth = 0;
+            continue;
         }
     }
 })();
