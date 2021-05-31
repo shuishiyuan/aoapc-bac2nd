e8e3bd3b42673b7f1a6c209c53e2db691243e201
shuishiyuan <liubinbupt@qq.com>
2021-05-31 08:27:00 +0800

    out put stream write communication in a short-cut OK.

--- ch3/ch3-pg8-exam4.js
+++ ch3/ch3-pg8-exam4.js
@@ -3,18 +3,22 @@ const fs = require("fs");
 const rs = fs.createReadStream("data/ch3-pg8-exam4.in");
 const ws = fs.createWriteStream("data/ch3-pg8-exam4.out");
 
+let output = '';
+
 const readline = require("readline");
 const rl = readline.createInterface({
     input: rs
 });
 rl.on('close', () => {
+    ws.write(output);
     process.exit();
 });
 
+
 (async () => {
     for await (let line of rl) {
-        ws.write(line);
         console.log(line);
+        output = output + line + '\n';
         if ('0' === line) {
             rl.close();
         }
